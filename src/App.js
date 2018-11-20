fcfb59c989f2fcc2443b06c29895493ffd5617ea
varun <varun@github.com>
2018-11-21 01:28:46 +0530

    Added drag drop and made UI responsive

--- src/App.js
+++ src/App.js
@@ -1,38 +1,15 @@
-import React, { Component } from 'react';
-// import { SketchPicker, PhotoshopPicker } from 'react-color'
-// var ls = require('local-storage');
+import React, { Component, PureComponent } from 'react';
 import * as ls from 'local-storage';
-// import logo from './logo.svg';
 import './App.css';
-// import '../node_modules/react-grid-layout/css/styles.css'
-// import '../node_modules/react-resizable/css/styles.css';
 
-// var ReactGridLayout = require('react-grid-layout');
- // import DatetimeRangePicker from 'react-datetime-range-picker';
-// import * as Datetime from 'react-datetime';
-
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import {Form, Input, FormGroup, Container, Label} from 'reactstrap';
-// import 'react-dates/initialize';
-// import 'react-dates/lib/css/_datepicker.css';
-// import {SingleDatePicker, DateRangePicker} from 'react-dates';
 
 class Block extends Component{
   constructor(props){
     super(props);
     this.state={data: []};
   }
-  componentDidMount(){
-    fetch(this.props.dataUrl)
-    .then(result=>result.json())
-    .then(posts=>{
-      this.setState({data: posts});
-    })
-  }
 
   isInTimeRange = (date, locaStorageIdentifier)=>{
-    // date<=ls.get('localLayout')[locaStorageIdentifier]['endDate'] && date >= ls.get('localLayout')[locaStorageIdentifier]['startDate'] 
-
     if(new Date(date)<=new Date(ls.get('localLayout')[locaStorageIdentifier]['endDate']) && new Date(date) >= new Date(ls.get('localLayout')[locaStorageIdentifier]['startDate'])){
       return true;
     }else {
@@ -43,7 +20,7 @@ class Block extends Component{
   render(){
     var heading = null;
     var locaStorageIdentifier = null;
-    if(this.state.data.length===0){
+    if(this.props.data.length===0){
       return (<p>Loading...</p>)
     }else {
       
@@ -61,28 +38,20 @@ class Block extends Component{
               <h3>{heading}</h3>
               <ol style={{marginRight: '5px'}}>
                 {
-                  // this.state.data.map((post, index)=>{
-                  //   var n = post.text.split(" ");
-                  //   var url = n.splice(-1);
-                  //   var content = n.join(" ");
-                  //   var date = new Date(post.created_at);
-                  //   return <li key={index.toString()} className="post"><div><a href={url} target="_blank" rel="noopener noreferrer">{content}</a><div className="date">{date.toDateString()}</div><hr/> </div></li>
-                  // })
-
                   function(){
                     var num = ls.get('localLayout')?ls.get('localLayout')[locaStorageIdentifier].count : 30;
                     var arr = [];
                     for(var i=0; i<num; i++){
-                      var date = new Date(this.state.data[i].created_at);
+                      var date = new Date(this.props.data[i].created_at);
                       if(!ls.get('localLayout') || (ls.get('localLayout') && this.isInTimeRange(date, locaStorageIdentifier))){
-                        var n = this.state.data[i].text.split(" ");
+                        var n = this.props.data[i].text.split(" ");
                         var url = null;
                         var content;
                         var usrName = null;
                         if(n[0]==='RT' && n[1][0]==='@'){
                           // do this
-                          if(this.state.data[i]["retweeted_status"]["entities"]["urls"][0]){
-                            url=this.state.data[i]["retweeted_status"]["entities"]["urls"][0]["url"];
+                          if(this.props.data[i]["retweeted_status"]["entities"]["urls"][0]){
+                            url=this.props.data[i]["retweeted_status"]["entities"]["urls"][0]["url"];
                             // console.log(url); 
                           }
                           
@@ -92,10 +61,6 @@ class Block extends Component{
                            url = n.splice(-1);
                            content = n.join(" ");
                         }
-
-                        // var url = n.splice(-1);
-                        // var content = n.join(" ");
-                        // var date = new Date(this.state.data[i].created_at);
                         arr.push(<li key={i.toString()} className="post"><div><a href={url} target="_blank" rel="noopener noreferrer">{content}</a><div className="date"><span>{usrName}</span><span>{date.toDateString()}</span></div><hr/> </div></li>)
                       }
                       
@@ -110,7 +75,7 @@ class Block extends Component{
   }
 }
 
-class MyFirstGrid extends Component{
+class MyFirstGrid extends PureComponent{
   constructor(props){
     super(props);
     
@@ -119,25 +84,101 @@ class MyFirstGrid extends Component{
     // const makeschool = 'http://localhost:7890/1.1/statuses/user_timeline.json?count=30&screen_name=makeschool';
     // this.state={leftSide: ycombinator, middle: makeschool, rightSide: newsycombinator};
     // var localLayout = ls.get('localLayout');
-    this.state = {orderOfColumns: {'leftSide' :  ls.get('localLayout')?ls.get('localLayout')['left']:'ycombinator' , 
-                                  'middle' : ls.get('localLayout')?ls.get('localLayout')['middle']:'makeschool',
-                                  'rightSide' : ls.get('localLayout')?ls.get('localLayout')['right']:'newsycombinator'}, }
+
+    // this.state = {orderOfColumns: {'leftSide' :  ls.get('localLayout')?ls.get('localLayout')['left']:'ycombinator' , 
+    //                               'middle' : ls.get('localLayout')?ls.get('localLayout')['middle']:'makeschool',
+    //                               'rightSide' : ls.get('localLayout')?ls.get('localLayout')['right']:'newsycombinator'}, }
+
+     this.state = {orderOfColumns: [ls.get('localLayout')?ls.get('localLayout')['left']:'ycombinator' , 
+                                    ls.get('localLayout')?ls.get('localLayout')['middle']:'makeschool',
+                                    ls.get('localLayout')?ls.get('localLayout')['right']:'newsycombinator'],
+                  data: {'ycombinator': [], 'newsycombinator': [], 'makeschool': []} ,
+                  ycombinator: [],
+                  newsycombinator: [],
+                  makeschool: []
+                }
+    
   }
 
+  componentDidMount(){
+    var urlArr = ['http://localhost:7890/1.1/statuses/user_timeline.json?count=30&screen_name=ycombinator', 'http://localhost:7890/1.1/statuses/user_timeline.json?count=30&screen_name=makeschool', 'http://localhost:7890/1.1/statuses/user_timeline.json?count=30&screen_name=newsycombinator']
+    var dic = {'0': 'ycombinator', '1': 'makeschool', '2': 'newsycombinator'}
+    var tempObj={};
+
+    Promise.all(urlArr.map(url=>{
+      return fetch(url);
+    }))
+    .then(responses=>{
+      return Promise.all(responses.map(res=>res.json()))
+    })
+    .then(allThreeposts=>{
+      allThreeposts.forEach((posts, index)=>{
+        tempObj[dic[index].toString()]=posts
+      })
+      this.setState({data: tempObj});
+    })    
+  }
+
+  drag(ev) {
+    ev.dataTransfer.setData("text", ev.currentTarget.id);
+  }
+  drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+  // ev.target.appendChild(document.getElementById(data));
+    this.swap(data, ev.currentTarget.id);
+  }
+  allowDrop(ev) {
+    ev.preventDefault();
+  }
+  swap = (id1, id2)=>{
+    var tempArr = Array.from(this.state.orderOfColumns);
+    tempArr[parseInt(id1)] = this.state.orderOfColumns[parseInt(id2)];
+    tempArr[parseInt(id2)] = this.state.orderOfColumns[parseInt(id1)];
+    this.setState({orderOfColumns: tempArr}, function(){
+      var tempObject = {};
+      if(ls.get("localLayout")){
+        tempObject = ls.get("localLayout");
+      }else {
+      tempObject = {
+                        'skinColor': '#f2f2f2',
+                        'ycombinator': {'count': '30', 'position': 'l', 'startDate': new Date("11 nov 1900"), 'endDate': new Date()},
+                        'newsycombinator': {'count': '30', 'position': 'l', 'startDate': new Date("11 nov 1900"), 'endDate': new Date()},
+                        'makeschool': {'count': '30', 'position': 'l', 'startDate': new Date("11 nov 1900"), 'endDate': new Date()},
+                        'left': 'ycombinator',
+                        'middle': 'makeschool',
+                        'right': 'newsycombinator'
+                      }
+      }
+      var dic = {
+        "0": 'left',
+        "1": 'middle',
+        "2": 'right'
+      }
+      // ls.set('localLayout')
+      tempObject[dic[id1]] =  this.state.orderOfColumns[parseInt(id1)];
+      tempObject[dic[id2]] =  this.state.orderOfColumns[parseInt(id2)];
+      ls.set('localLayout', tempObject);
+    });
+  }
+
+
+
   render() {
+    
     // update localStorage
     var constUrlPart = 'http://localhost:7890/1.1/statuses/user_timeline.json?count=30&screen_name=';
     return (
         <div className="container" style={{backgroundColor: localStorage.getItem("overallSkin")?localStorage.getItem("overallSkin"): null} } >
-          <div style={{flex: '1'}}><Block dataUrl={constUrlPart + this.state.orderOfColumns.leftSide} /></div>
-          <div style={{flex: '1'}}><Block dataUrl={constUrlPart + this.state.orderOfColumns.middle} /></div>
-          <div style={{flex: '1'}}><Block dataUrl={constUrlPart + this.state.orderOfColumns.rightSide} /></div>
+          <div id="0" style={{flex: '1'}} draggable="true" onDragStart={(event)=>this.drag(event)} onDrop={(event)=>this.drop(event)} onDragOver={(event)=>this.allowDrop(event)} ><Block dataUrl={constUrlPart + this.state.orderOfColumns[0]} data={this.state.data[this.state.orderOfColumns[0]]}/></div>
+          <div id="1" style={{flex: '1'}}  draggable="true" onDragStart={(event)=>this.drag(event)} onDrop={(event)=>this.drop(event)} onDragOver={(event)=>this.allowDrop(event)}><Block dataUrl={constUrlPart + this.state.orderOfColumns[1]} data={this.state.data[this.state.orderOfColumns[1]]}/></div>
+          <div id="2" style={{flex: '1'}}  draggable="true" onDragStart={(event)=>this.drag(event)} onDrop={(event)=>this.drop(event)} onDragOver={(event)=>this.allowDrop(event)}><Block dataUrl={constUrlPart + this.state.orderOfColumns[2]} data={this.state.data[this.state.orderOfColumns[2]]}/></div>
        </div>
       )
   }
 };
 
-class EditLayoutForm extends Component{
+class EditLayoutForm extends PureComponent{
   state= {focusedInput: null};
 
   isValidCount=(countNum)=>{
@@ -152,11 +193,11 @@ class EditLayoutForm extends Component{
          && ! isNaN( parseInt(sNum.substring(1, sNum.length), 16) );
     }
   isValidColor=(skinColor)=>{
-    console.log("hexcolor func value: "+ this.isHexaColor(skinColor))
+    // console.log("hexcolor func value: "+ this.isHexaColor(skinColor))
     if(skinColor!=="#000000" && this.isHexaColor(skinColor)){
       return true;
     }else {
-      console.log("I was printed: "+ skinColor)
+      // console.log("I was printed: "+ skinColor)
       return false;
     }
   }
@@ -234,59 +275,59 @@ class EditLayoutForm extends Component{
     }
 
     ls.set("localLayout", tempObject);
-    console.log(ls.get("localLayout").ycombinator);
+    // console.log(ls.get("localLayout").ycombinator);
   }
 
   render(){
     return (
       <form className='form'>
-        <div className='formLine'>
+        {/* <div className='formLine'>
           <span style={{flex: '1'}}></span>
           <h1 style={{flex: '1'}}>Date Range</h1>
           <h1 style={{flex: '1'}}>Number of Posts</h1>
           <h1 style={{flex: '1'}}>Position of Posts</h1>
-        </div>
+        </div> */}
         <div className="formLine">
           <h2 style={{marginRight: '5px', flex: '1'}}>Y Combinator:</h2>
-          <MyDatePicker locaStorageIdentifier={'ycombinator'} />
+          <div style={{flex: '1'}}><MyDatePicker locaStorageIdentifier={'ycombinator'} /></div>
           <div style={{flex: '1'}}>
-            <input type="number" id="ycombinatorCount" min="1" max="30" />
+            <input type="number" style={{width: "50px", marginTop: "10px"}} id="ycombinatorCount" min="1" max="30" placeholder="num" />
           </div>
-          <div className="positionInput" style={{flex: '1'}}>
+          {/* <div className="positionInput" style={{flex: '1'}}>
            <div className="positionRadioEntry"><label style={{marginRight: '5px'}}>L:</label> <input type="radio" name="ycombinator"  value="left" /></div> 
            <div className="positionRadioEntry"><label style={{marginRight: '5px'}}>M:</label ><input type="radio" name="ycombinator"  value="middle" /></div> 
            <div className="positionRadioEntry"><label style={{marginRight: '5px'}}>R:</label> <input type="radio" name="ycombinator"  value="right" /></div> 
-          </div>
+          </div> */}
         </div>
         <div className="formLine">
           <h2 style={{marginRight: '5px', flex: '1'}}>Hacker News:</h2>
-          <MyDatePicker  locaStorageIdentifier={'newsycombinator'}/>
+          <div style={{flex: '1'}}><MyDatePicker  locaStorageIdentifier={'newsycombinator'}/></div>
           <div style={{flex: '1'}}>
-            <input type="number" id="newsycombinatorCount" min="1" max="30" />
+            <input type="number" style={{width: "50px", marginTop: "10px" }} id="newsycombinatorCount" min="1" max="30" placeholder="num" />
           </div>
-          <div className="positionInput" style={{flex: '1'}}>
+          {/* <div className="positionInput" style={{flex: '1'}}>
            <div className="positionRadioEntry"><label style={{marginRight: '5px'}}>L:</label> <input type="radio" name="newsycombinator"  value="left" /></div> 
            <div className="positionRadioEntry"><label style={{marginRight: '5px'}}>M:</label ><input type="radio" name="newsycombinator"  value="middle" /></div> 
            <div className="positionRadioEntry"><label style={{marginRight: '5px'}}>R:</label> <input type="radio" name="newsycombinator"  value="right" /></div> 
-          </div>
+          </div> */}
         </div>
         <div className="formLine">
           <h2 style={{marginRight: '5px', flex: '1'}}>Make School:</h2>
-          <MyDatePicker locaStorageIdentifier={'makeschool'}/>
+          <div style={{flex: '1'}}><MyDatePicker locaStorageIdentifier={'makeschool'}/></div>
           <div style={{flex: '1'}}>
-            <input type="number" id="makeschoolCount" min="1" max="30" />
+            <input type="number" style={{width: "50px", marginTop: "10px"}} id="makeschoolCount" min="1" max="30" placeholder="num" />
           </div>
-          <div className="positionInput" style={{flex: '1'}}>
+          {/* <div className="positionInput" style={{flex: '1'}}>
            <div className="positionRadioEntry"><label style={{marginRight: '5px'}}>L:</label> <input type="radio" name="makeschool"  value="left" /></div> 
            <div className="positionRadioEntry"><label style={{marginRight: '5px'}}>M:</label ><input type="radio" name="makeschool"  value="middle" /></div> 
            <div className="positionRadioEntry"><label style={{marginRight: '5px'}}>R:</label> <input type="radio" name="makeschool"  value="right" /></div> 
-          </div>
+          </div> */}
         </div>
         <div className='formLine'> 
           <h2 style={{marginRight: '5px', flex: '1'}}>Pick Skin Color: </h2>
-          <div style={{flex: '1'}}><input style={{width: '50%'}} id="skinColor" type='color' /></div>
+          <div style={{flex: '1'}}><input style={{width: '50px'}} id="skinColor" type='color' /></div>
           <span style={{flex: '1'}}></span>
-          <span style={{flex: '1'}}></span>
+          {/* <span style={{flex: '1'}}></span> */}
         </div>
         <div className='formLine'>
           <input className="button" onClick={this.layoutSubmission} type="submit" />
@@ -296,49 +337,32 @@ class EditLayoutForm extends Component{
   }
 }
 
-class MyDatePicker extends Component{
+class MyDatePicker extends PureComponent{
   state={};
 
   render(){
     var startDateId = this.props.locaStorageIdentifier+'startDate';
     var endDateId = this.props.locaStorageIdentifier + 'endDate'
     return (
-      // <DateRangePicker
-      //   startDate={this.state.startDate} // momentPropTypes.momentObj or null,
-      //   startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
-      //   endDate={this.state.endDate} // momentPropTypes.momentObj or null,
-      //   endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
-      //   onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
-      //   focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
-      //   onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
-      // />
       <div>
-      <input type="date" id={startDateId}/>
-      <span> -> </span>
-      <input type="date" id  = {endDateId} />
+        <input type="date" id={startDateId} style={{marginLeft: '10px'}} />
+        <input type="date" id  = {endDateId} style={{marginLeft: '10px'}} />
       </div>
     )
   }
 }
 
-class App extends Component{
+class App extends PureComponent{
 
-  state = {editLayout: false};
+  state = {editLayout: {value: false}};
 
   handleCheckBoxClick = (e)=>{
-    this.setState({editLayout: !this.state.editLayout})
+    // this.setState({editLayout: !this.state.editLayout})
+    var temp = {value: !this.state.editLayout.value};
+    this.setState({editLayout: temp});
   }
 
   render() {
-    // Hack: For better user exp.
-    // If localStrorage have custom layout than set endDate to current date on every reload.
-    // if(ls.get('localLayout')){
-    //   var tempObj = ls.get('localLayout');
-    //   tempObj['ycombinator']['endDate']= new Date();
-    //   tempObj['newsycombinator']['endDate']= new Date();
-    //   tempObj['makeschool']['endDate']= new Date();
-    //   ls.set('localLayout', tempObj);
-    // }
     return (
       <div className="App" style={{backgroundColor: ls.get('localLayout')?ls.get('localLayout').skinColor: '#f2f2f2'}}>
         <div className="upper-bar">
@@ -348,16 +372,11 @@ class App extends Component{
             <span className="slider round"></span>
           </label>
         </div>
-        
-      {
-        function(){
-          if(!this.state.editLayout){
-            return <MyFirstGrid />
-          }else {
-            return <EditLayoutForm />
-          }
-        }.bind(this)()
-      }
+        <div>
+        {
+          this.state.editLayout.value?<EditLayoutForm />:<MyFirstGrid />
+        }
+        </div>
       </div>
      )     
 
